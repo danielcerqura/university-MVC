dc042f1ec5a636e0d07c4b6b515960afd1dc0786
danielcerqura <61717917+danielcerqura@users.noreply.github.com>
2020-03-12 20:05:15 -0500

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,7 +1,7 @@
 <?php
 class Conectar{
     public static function conexion(){
-        $conexion=new mysqli("remotemysql.com", "7tK58L6A24", "4iULrSz7Xe", "7tK58L6A24");
+        $conexion=new mysqli("remotemysql.com", "EjceBhVd0B", "Pz00j4utJo", "EjceBhVd0B");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
